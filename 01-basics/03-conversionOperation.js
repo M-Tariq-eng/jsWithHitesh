2ed067227990ea2d8b5a181c4371e7e3b8698c45
Muhammad Tariq <muhammadtariqhts@gmail.com>
2026-04-28 13:05:27 +0000

    Conversion of DataTypes

---
+++ 01-basics/03-conversionOperation.js
@@ -0,0 +1,39 @@
+// let score = "33abc"
+// let score = true
+
+// console.log(score);
+// console.log(typeof score);
+
+// let convertIntoNumber = Number(score)
+
+// console.log(typeof convertIntoNumber);
+// console.log(convertIntoNumber);
+
+//"33"  => 33
+//"33abc" => NaN
+//true => 1; false => 0
+
+
+let isLogedIn = 0
+
+let convert = Boolean(isLogedIn)
+
+// console.log(typeof convert);
+// console.log(convert);
+
+//1 => true; 0 => false
+// "" => false
+// "Hi" => true
+
+
+let number = 33;
+
+let stringNum = String(number)
+
+console.log(stringNum);
+console.log(typeof stringNum);
+
+
+
+
+
