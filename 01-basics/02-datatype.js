d7e7c803dd859a1fefb98ffbfeab08474efd38a6
Muhammad Tariq <muhammadtariqhts@gmail.com>
2026-04-28 12:39:19 +0000

    DataTypes

---
+++ 01-basics/02-datatype.js
@@ -0,0 +1,32 @@
+"Use strict";
+
+// alert(3 + 4) we are working on node not the browser
+
+console.log(3 +
+    3
+)  //Avoid this because of code readabilty
+
+let name = "Tariq"
+let age = 21;
+let isStudent = true
+
+console.log(name);
+
+//There are 7 data types in premitive 
+//Number:  2
+//Begint: Very long number 
+//String: ""
+//Boolean: true/false
+//undefined: when we declear the varible but we didn't assign the value
+//null: when we decide the value is must be null like there is nothing in this variable
+
+
+// Object 
+
+console.log(typeof 1);  //Number
+console.log(typeof "");  //String
+console.log(typeof true); //Boolean
+console.log(typeof undefined); //Undefined
+console.log(typeof null); //Object because of JS bug
+
+
