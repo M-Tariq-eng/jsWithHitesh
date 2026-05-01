874b2365b33e7d3cf84cb36aadcfc07ce40e4ee6
Muhammad Tariq <muhammadtariqhts@gmail.com>
2026-05-01 12:40:16 +0000

    Number + Msth

---
+++ 01-basics/05-number- and-math.js
@@ -0,0 +1,32 @@
+const score = 100;
+console.log(score);
+
+
+const num = new Number(300)
+console.log(num);
+console.log(num.toString());
+console.log(num.toString().length);
+
+let number = 200
+console.log(number.toFixed(2));
+
+
+let otherNumber = 23.9873
+console.log(otherNumber.toPrecision(3));
+
+
+
+
+
+
+
+
+const hundred = 1000000;
+console.log(hundred.toLocaleString());
+console.log(hundred.toLocaleString('en-IN'));
+
+
+
+
+//+++++++++++++++++++++++++++++++MATH++++++++++++++++++++++++++++++++++++==
+console.log(Math);
