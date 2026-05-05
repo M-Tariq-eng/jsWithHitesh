445fc1ec495deb4184414b824d4090a3393fbfff
Muhammad Tariq <muhammadtariqhts@gmail.com>
2026-05-05 15:23:35 +0000

    Math

--- 01-basics/05-number- and-math.js
+++ 01-basics/05-number- and-math.js
@@ -1,18 +1,18 @@
 const score = 100;
-console.log(score);
+// console.log(score);
 
 
 const num = new Number(300)
-console.log(num);
-console.log(num.toString());
-console.log(num.toString().length);
+// console.log(num);
+// console.log(num.toString());
+// console.log(num.toString().length);
 
 let number = 200
-console.log(number.toFixed(2));
+// console.log(number.toFixed(2));
 
 
 let otherNumber = 23.9873
-console.log(otherNumber.toPrecision(3));
+// console.log(otherNumber.toPrecision(3));
 
 
 
@@ -22,11 +22,33 @@ console.log(otherNumber.toPrecision(3));
 
 
 const hundred = 1000000;
-console.log(hundred.toLocaleString());
-console.log(hundred.toLocaleString('en-IN'));
+// console.log(hundred.toLocaleString());
+// console.log(hundred.toLocaleString('en-IN'));
 
 
 
 
 //+++++++++++++++++++++++++++++++MATH++++++++++++++++++++++++++++++++++++==
-console.log(Math);
+// console.log(Math);
+
+// console.log(Math.floor(4.9));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+
+
+let numb = Math.floor(Math.random() * 6) + 1
+console.log(numb);
+
+
+
+
+
+
+
+let n = 7;
+
+
+// console.log(n * n )
+// console.log(n * n * n)
+// console.log(n / 2)
+
