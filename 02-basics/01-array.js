1f4838e03cb55d06a6ce231290b39897d883fbe7
Muhammad Tariq <muhammadtariqhts@gmail.com>
2026-05-06 19:26:46 +0000

    arr part 1

---
+++ 02-basics/01-array.js
@@ -0,0 +1,59 @@
+let arr = [1, 2, 3, 4, 5]
+
+let myarr = new Array("a", "b", "c", "d")
+
+
+// console.log(arr[2]);
+// console.log(myarr[1]);
+
+
+// Array Methods
+
+// arr.push(6)
+// console.log(arr);
+
+// arr.pop()
+// console.log(arr);
+
+
+// arr.unshift(3)
+// arr.shift()
+// console.log(arr);
+
+
+// let inc = myarr.includes('f')
+// console.log(inc);
+
+
+// console.log(myarr.indexOf('c'))
+
+
+
+const newarr = myarr.join()
+
+// console.log(newarr);
+// console.log(myarr);
+
+
+//slice and splice
+console.log("A " , myarr);
+let ar = myarr.slice(1, 3)
+console.log(ar);
+console.log("B " , myarr);
+
+const myarr2 = myarr.splice(1, 3)
+console.log("C " , myarr);
+
+
+console.log(myarr2);
+
+
+
+
+
+
+
+
+
+
+
