60cc278346e75c96a91ca0554f5e494d23d11901
Muhammad Tariq <muhammadtariqhts@gmail.com>
2026-05-07 14:33:17 +0000

    Array part 2

---
+++ 02-basics/02-arrays.js
@@ -0,0 +1,16 @@
+let boys = ["Azan", "Inam", "Rahat", "Sami"]
+
+let girls = ["Mahnoor", "Rimsha", "Areeba", "Komal"]
+
+// boys.push(girls)
+
+// console.log(boys);
+
+let all = boys.concat(girls)
+
+// console.log(all);
+
+let spread = [...boys, ...girls]
+console.log(spread);
+
+
